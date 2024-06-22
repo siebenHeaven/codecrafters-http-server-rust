c70449b32546f067802309633b1b35f54b864415
Anup Mahindre <anup.mahindre.7@gmail.com>
2024-06-22 15:32:20 +0530

    Pass stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
-use std::io::Write;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -17,6 +17,21 @@ fn main() {
 }
 
 fn handle_client(mut stream: TcpStream) {
-    println!("Accepted\n");
-    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("{}", http_request[0]);
+    match http_request[0].as_str() {
+        "GET / HTTP/1.1" => 
+                stream
+                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                    .unwrap(),
+            _ => 
+                stream
+                    .write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                    .unwrap(),
+    }
 }
