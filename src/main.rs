c3676c3960418268ca666fa498f898c8a3aef8ab
Anup Mahindre <anup.mahindre.7@gmail.com>
2024-06-22 15:17:38 +0530

    Pass stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::Write;
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_client(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    println!("Accepted\n");
+    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
 }
