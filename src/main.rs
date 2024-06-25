5311437d016a40f7d1d98afd9ddac57314c6a7e6
Anup Mahindre <anup.mahindre.7@gmail.com>
2024-06-25 21:10:08 +0530

    User-agent header pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 
@@ -21,30 +22,39 @@ enum RequestType {
     Post,
 }
 
-struct HttpRequest<'a> {
-    typ: RequestType,
-    path: &'a str,
+impl Default for RequestType {
+    fn default() -> Self {
+        RequestType::Get
+    }
 }
 
-impl Default for HttpRequest<'_> {
-    fn default() -> Self {
-        Self {
-            typ: RequestType::Get,
-            path: Default::default(),
-        }
-    }
+#[derive(Default)]
+struct HttpRequest {
+    typ: RequestType,
+    path: String,
+    headers: HashMap<String, String>,
+    body: Vec<String>,
 }
 
 fn parse(http_request: &Vec<String>) -> HttpRequest {
     let mut http_request_out: HttpRequest = Default::default();
+    let mut req_it = http_request.iter();
 
-    let mut it = http_request[0].split_whitespace();
+    let mut it = req_it.next().unwrap().split_whitespace();
 
     if it.next().unwrap() == "POST" {
         http_request_out.typ = RequestType::Post;
     }
 
-    http_request_out.path = it.next().unwrap();
+    http_request_out.path = it.next().unwrap().to_string();
+
+    loop {
+        if let Some(Some((header, val))) = req_it.next().map(|line| line.split_once(':')) {
+            http_request_out.headers.insert(header.to_string(), val.trim().to_string());
+        } else {
+            break;
+        }
+    }
 
     http_request_out
 }
@@ -55,13 +65,16 @@ fn handle_get_request(http_request: HttpRequest) -> String {
     }
     let path_components: Vec<_> = http_request.path.split('/').collect();
     match path_components[1] {
-        "echo" => {
-            format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                path_components[2].len(),
-                path_components[2]
-            )
-        }
+        "echo" => format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            path_components[2].len(),
+            path_components[2]
+        ),
+        "user-agent" => format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            http_request.headers["User-Agent"].len(),
+            http_request.headers["User-Agent"]
+        ),
         _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
     }
 }
