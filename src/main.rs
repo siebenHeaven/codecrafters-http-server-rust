ba02cdfd4865d38697dd3a4dda869adaf4fd693a
Anup Mahindre <anup.mahindre.7@gmail.com>
2024-06-29 19:45:16 +0530

    Add files endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,43 @@ use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
+#[derive(Clone)]
+struct Args {
+    directory: String,
+}
+
+impl Default for Args {
+    fn default() -> Self {
+        Self {
+            directory: "./".to_string(),
+        }
+    }
+}
+
+fn parse_args() -> Args {
+    let mut args = Args::default();
+    let mut args_iter = std::env::args().skip(1);
+
+    match args_iter.next().unwrap_or("".to_string()).as_str() {
+        "--directory" => {
+            args.directory = args_iter.next().unwrap().to_string();
+        }
+        _ => {},
+    }
+
+    args
+}
+
 fn main() {
+    let args = parse_args();
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
+        let cloned_args = args.clone();
         match stream {
             Ok(stream) => {
-                thread::spawn(|| handle_client(stream));
+                thread::spawn(|| handle_client(stream, cloned_args));
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -34,7 +64,7 @@ struct HttpRequest {
     typ: RequestType,
     path: String,
     headers: HashMap<String, String>,
-    body: Vec<String>,
+    _body: Vec<String>,
 }
 
 fn parse(http_request: &Vec<String>) -> HttpRequest {
@@ -49,18 +79,16 @@ fn parse(http_request: &Vec<String>) -> HttpRequest {
 
     http_request_out.path = it.next().unwrap().to_string();
 
-    loop {
-        if let Some(Some((header, val))) = req_it.next().map(|line| line.split_once(':')) {
-            http_request_out.headers.insert(header.to_string(), val.trim().to_string());
-        } else {
-            break;
-        }
+    while let Some(Some((header, val))) = req_it.next().map(|line| line.split_once(':')) {
+        http_request_out
+            .headers
+            .insert(header.to_string(), val.trim().to_string());
     }
 
     http_request_out
 }
 
-fn handle_get_request(http_request: HttpRequest) -> String {
+fn handle_get_request(http_request: HttpRequest, args: Args) -> String {
     if http_request.path == "/" {
         return "HTTP/1.1 200 OK\r\n\r\n".to_string();
     }
@@ -76,11 +104,24 @@ fn handle_get_request(http_request: HttpRequest) -> String {
             http_request.headers["User-Agent"].len(),
             http_request.headers["User-Agent"]
         ),
+        "files" => {
+            if let Ok(f) =
+                std::fs::read_to_string(format!("{}{}", args.directory, path_components[2]))
+            {
+                format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+            f.len(),
+            f
+        )
+            } else {
+                "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+            }
+        }
         _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, args: Args) {
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
         .lines()
@@ -91,7 +132,7 @@ fn handle_client(mut stream: TcpStream) {
     let http_request: HttpRequest = parse(&http_request);
 
     let response = match http_request.typ {
-        RequestType::Get => handle_get_request(http_request),
+        RequestType::Get => handle_get_request(http_request, args),
         RequestType::Post => todo!(),
     };
 
